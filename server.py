91b8c56e29e4f7ce73983ee53cd3dfcb70403713
SomeoneAlt-86 <76504556+SomeoneAlt-86@users.noreply.github.com>
2024-06-02 09:10:19 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,10 @@
+# secure_server.py
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from ssl import PROTOCOL_TLS_SERVER, SSLContext
+
+ssl_context = SSLContext(PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain("cert.pem", "private.key")
+server = HTTPServer(("0.0.0.0", 443), SimpleHTTPRequestHandler)
+server.socket = ssl_context.wrap_socket(server.socket, server_side=True)
+server.serve_forever()
